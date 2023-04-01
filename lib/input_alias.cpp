a6f410b7208b6cb46bf7d6bca2017224b79954b2
louiml <99025570+funmmer@users.noreply.github.com>
2023-04-01 22:09:22 +0300

    Update input_alias.cpp

--- lib/input_alias.cpp
+++ lib/input_alias.cpp
@@ -1,9 +1,7 @@
 #include "input_alias.h"
 
-// Temporarily undefine the cin macro
 #undef cin
 
 InputWrapper input(std::cin.rdbuf());
 
-// Redefine the cin macro
 #define cin typename prohibit_cin<std::istream>::type
